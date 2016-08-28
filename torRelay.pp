889bac4c3f1a1154d1ddc21b5b7365080eab5ce2
Chelsea H. Komlo <chelsea.komlo@gmail.com>
2016-08-28 15:04:47 -0500

    add docker dependency

--- torRelay.pp
+++ torRelay.pp
@@ -6,6 +6,10 @@ package { 'make':
         ensure => present,
 }
 
+package { 'docker':
+        ensure => present,
+}
+
 exec { 'source':
         command => "/usr/bin/git clone https://github.com/chelseakomlo/tor-relay.git"
 }
